567e0412dd0c5dc8ca44c19991694ca6ae36db0f
KANGSI_LAPTOP\Weikang <weikang_hu90@hotmail.com>
2016-09-21 15:21:04 +0200

    AI commit

---
+++ Life of Tyr/Assets/Scripts/AI/Behaviours/BasicAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAI : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void StartBehaviour() {
+	
+	}
+
+    // Update is called once per frame
+    public virtual void UpdateBehaviour () {
+	
+	}
+
+    public virtual void EndBehaviour()
+    {
+
+    }
+}
---
+++ Life of Tyr/Assets/Scripts/AI/EnemyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    BasicAI ai = new DefaultAI();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ai.UpdateBehaviour();
+	}
+}
